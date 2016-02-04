f2ce7df0846e262ab0d51d71a23a208dc71dc3fa
Andrea Brandi <starise@gmail.com>
2016-02-04 20:32:27 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dotfiles by starise
+
+Install Gnu/Linux dotfiles for Debian or Ubuntu.
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# author: starise
+
+# Get current directory
+export DOTFILES_DIR
+DOTFILES_DIR="$(cd $(dirname "$1") && pwd -P)"
+
+# Symlink configuration files
+echo "Installing Dotfiles..."
+ln -sfv "$DOTFILES_DIR/.bash_aliases" ~
+ln -sfv "$DOTFILES_DIR/.bash_functions" ~
+ln -sfv "$DOTFILES_DIR/.bashrc" ~
+ln -sfv "$DOTFILES_DIR/.editorconfig" ~
+
+if [ -f ${DOTFILES_DIR}/.gitconfig.${USER} ]; then
+  ln -sfv "$DOTFILES_DIR/.gitconfig.$USER" ~
+else
+  ln -sfv "$DOTFILES_DIR/.gitconfig" ~
+fi
